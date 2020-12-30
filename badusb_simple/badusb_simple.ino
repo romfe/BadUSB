bb2288118bbb708a5cf30a5727996977c7b18610
romfe <romfe89@gmail.com>
2020-12-30 15:48:08 -0300

    Reworking the logic

--- badusb_simple/badusb_simple.ino
+++ badusb_simple/badusb_simple.ino
@@ -1,21 +1,14 @@
-//#include <HID.h>
+#include <HID.h>
 #include <Keyboard.h>
 void setup() {
+  delay(400);
   Keyboard.begin();
   Keyboard.press(KEY_LEFT_GUI);
-  delay(500); // the processor need time to register key press
-  Keyboard.press("r");
-  Keyboard.releaseAll();
-  delay(500); // approximate time needed to process our intruction
-  Keyboard.write("cmd");
-  delay(100);
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_SHIFT);
-  Keyboard.press(KEY_RETURN);
+  delay(200);
   Keyboard.releaseAll();
   Keyboard.end();
 }
 
 void loop() {
-  // vazio
+  // vazio / empty
 }
